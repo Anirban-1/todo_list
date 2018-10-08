dc7a2d5eff27f9a7d48f4d8d99be112f1cba8adc
Anirban Acharya <acharya.anirban9@gmail.com>
2018-10-08 09:55:02 +0530

    develop/feat-b updated application.properties file Added annotation to ToDoDTO

--- src/main/java/com/todolist/app/model/ToDoDTO.java
+++ src/main/java/com/todolist/app/model/ToDoDTO.java
@@ -1,7 +1,9 @@
 package com.todolist.app.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ToDoDTO extends CrudRepository<ToDo, Integer> {
 	ToDo findByUserId(int userId);
 }
